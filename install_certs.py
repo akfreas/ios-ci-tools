896cbb8b5d891e17c35054ae99e570a769222979
Alex Freas <akfreas@gmail.com>
2013-10-21 17:10:09 -0400

    Using travis global KEY_PASSWORD in cert install script.

--- install_certs.py
+++ install_certs.py
@@ -9,6 +9,7 @@ from tempfile import mkdtemp
 
 aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
 aws_secret_access_key = os.getenv("AWS_SECRET_KEY")
+key_password = os.getenv("KEY_PASSWORD")
 s3_bucket_name = "travis-assets"
 certs_key_name = "certs.zip"
 
@@ -31,7 +32,7 @@ provisioning_profile_dir = "~/Library/MobileDevice/Provisioning Profiles"
 os.system("security create-keychain -p travis ios-build.keychain")
 os.system("security import %s/apple.cer ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign" % temp_path)
 os.system("security import %s/iPhone-distribution.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign")
-os.system("security import %s/iPhone-distribution.p12 -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign")
+os.system("security import %s/iPhone-distribution.p12 -k ~/Library/Keychains/ios-build.keychain -p %s -T /usr/bin/codesign" % key_password)
 os.mkdir(provisioning_profile_dir)
 shutil.copy("iOS_Team_Provisioning_Profile_48L8E2ESF8comridechargedrivermagic.mobileprovision", provisioning_profile_dir) 
 os.rmdir(temp_path)
